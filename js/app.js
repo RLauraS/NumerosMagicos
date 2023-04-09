8a2c3f2e19ceb97aa1890bbe99abece0f68bdff2
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-09 20:23:19 -0300

    Se finaliza el ejercicio y se corrige el navbar

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 /*Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
  */
-
-
 let formulario = document.querySelector("form");
 formulario.addEventListener('submit', leerDatoFormulario);
 
@@ -17,12 +15,15 @@ function leerDatoFormulario(e) {
   if (datoIngresado === numeroMagico) {
     alert('¡Felicidades, adivinaste el número mágico!');
   } else if (datoIngresado < numeroMagico) {
-    alert('El número que ingresaste es menor al número mágico.');
+    alert('El número que ingresaste es menor al número mágico. El número era: '+numeroMagico);
   } else {
-    alert('El número que ingresaste es mayor al número mágico.');
+    alert('El número que ingresaste es mayor al número mágico. El número era: '+numeroMagico);
   }
+
+  input.value = ''; // Limpiar el contenido del input
 }
+
 function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
